a439f896733bbb5710fa5c0ca6dc8d235cd44fed
viper4052 <57199130+viper4052@users.noreply.github.com>
2022-10-22 10:28:05 -0600

    creadas clases camara y tunel. cambio de nombre de archivo arbol

---
+++ Camara.h
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+#include "Node.h"
+
+using namespace std;
+
+template <class T>
+class Camara
+{
+private:
+    int potencialMinado;
+    int unidadesMineral;
+    int profundidadDeCamara;
+
+
+public:
+    int getPotencialMinado(){
+        return this->potencialMinado;
+    }
+
+    // las camaras de mayor potencial SIEMPRE estan a la IZQUIERDA
+    void setPotencialMinado(){ // define el potencial de minado automaticamente dependiendo de 
+                               // distancia y cantidad de mineral dentro de camara.
+        this->potencialMinado = this->getUnidadesMineral() * this->getProfundidadCamara();
+        return;
+    }
+
+    int getUnidadesMineral(){
+        return this->unidadesMineral;
+    }
+
+    void setUnidadesMineral(){
+        int randomUnidades;
+        srand(time(0));  // inicia semilla generadora de numero
+        for(int i=0;i<10;i++) // hace 10 calculos para obtener un random
+            randomUnidades = (rand() % 20); 
+        this->unidadesMineral = randomUnidades+1 // +1 para asegurarse que nunca va a ser 0
+    }
+
+
+    int getProfundidadCamara(){
+        return this->profundidadDeCamara;
+    }
+
+    void setProfundidadCamara(int pProfundidad){
+        this->profundidadDeCamara = pProfundidad;
+    }
+};
+
